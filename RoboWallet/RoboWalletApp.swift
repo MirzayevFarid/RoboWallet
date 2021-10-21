02dccb72243aa6d51a440811d84292c329a05551
Farid Mirzayev <mirzayevfarid@yahoo.com>
2021-10-22 01:15:34 +0300

    Initial Commit

---
+++ RoboWallet/RoboWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoboWalletApp.swift
+//  RoboWallet
+//
+//  Created by Mirzayev Farid on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct RoboWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
